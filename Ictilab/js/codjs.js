1cf1f554cdcaab781a503956d5ae15e9fa00d558
Maxwell Fernandes Pimentel Júnior <41161988+MaxJunior26@users.noreply.github.com>
2025-07-25 18:44:53 -0300

    Atualização

--- Ictilab/js/codjs.js
+++ Ictilab/js/codjs.js
@@ -2,7 +2,8 @@ console.log("Teste Inicial");
 
 const usuarios = {
   macaxwell: "senha123",
-  sabrinei: "senha456"
+  ingridiane: "senha456",
+  cingrid: "senha789"
 };
 
 function validacao() {
@@ -12,22 +13,23 @@ function validacao() {
   if (usuarios[login] && usuarios[login] === senha) {
 	window.location.href = "../Ictilab/pagina_inicial.html";
   } else {
-    alert("Login ou senha inválidos.");
+	alert("Login ou senha inválidos.");
   }
 
 	//https://youtu.be/1tjuhCdWPB4
 }
 
 function changeDarkMode() {
-  document.body.classList.toggle("dark-mode");
-  document.documentElement.classList.toggle("dark-mode");
+	document.body.classList.toggle("dark-mode");
+	document.documentElement.classList.toggle("dark-mode");
 
-  const claroEscuro = document.getElementById("claro-escuro");
-  const imagemicticlaro = document.getElementById("logo-ictilab-escuro");
-  const modoEscuroAtivo = document.body.classList.contains("dark-mode");
-  const textoReforco = document.getElementById("reforco");
+	const claroEscuro = document.getElementById("claro-escuro");
+	const imagemIctiClaro = document.getElementById("logo-ictilab-escuro");
+	const modoEscuroAtivo = document.body.classList.contains("dark-mode"); //valida se o modo escuro está ativo
+	document.querySelectorAll('.azul').forEach(el => {
+		el.style.color = modoEscuroAtivo ? "#add8e6" : "#125bb3";
+	})
 
   claroEscuro.src = modoEscuroAtivo ? "assets/sol.png" : "assets/lua.png";
-  textoReforco.color = modoEscuroAtivo ? "#add8e6" : "#125bb3";
-  imagemicticlaro.src = modoEscuroAtivo ? "assets/ictilab-claro.png" : "assets/ictilab.png";
+  imagemIctiClaro.src = modoEscuroAtivo ? "assets/ictilab-claro-removebg-preview.png" : "assets/ictilab.png";
 }
